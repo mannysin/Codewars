23e87472d80261459be0c0557d2e7a99667769f8
mannysin <sinclair2610@gmail.com>
2019-01-09 15:24:20 -0500

    Solution for Transportation on vacation

--- 8kyu/transportationonvacation.js
+++ 8kyu/transportationonvacation.js
@@ -11,23 +11,71 @@ Write a code that gives out the total amount for different days(d).
 
 My approach:
 
+Set a variable set to the base cost.
+
+Set up different conditionals based on days rented.
+
+Finally, return the value depending on the condition.
 
 */
 
 function rentalCarCost(d) {
-    
-
-
-
-
-
-
+    const base = 40
+        if (d >= 7){
+            return d * base - 50
+        }
+        if (d >= 3) {
+            return d * base - 20
+        }
+        if (d < 3) {
+            return d * base
+        }
 }
 
-  /*
-  Best Practice:
 
+/* Test Cases
+Test.describe("Regular Tests:", function() {
+  Test.it("Should work for under 3 days:", function() {
+    Test.assertEquals(rentalCarCost(1), 40);
+    Test.assertEquals(rentalCarCost(2), 80);
+  });
+  Test.it("Should work for under 7 days:", function() {
+    Test.assertEquals(rentalCarCost(3), 100);
+    Test.assertEquals(rentalCarCost(4), 140);
+    Test.assertEquals(rentalCarCost(5), 180);
+    Test.assertEquals(rentalCarCost(6), 220);
+  });
+  Test.it("Should work for 7 or more days:", function() {
+    Test.assertEquals(rentalCarCost(7), 230);
+    Test.assertEquals(rentalCarCost(8), 270);
+    Test.assertEquals(rentalCarCost(9), 310);
+    Test.assertEquals(rentalCarCost(10), 350);    
+  });
+});
+*/
 
+rentalCarCost(4) //returned 140
 
+/*
+Best Practice:
 
-  */
+function baseCost(days, rate) {
+  return days * rate;
+}
+
+function discountRate(days) {
+  if ( days >= 7 ) {
+    return 50;
+  }
+  else if ( days >= 3 ) {
+    return 20;
+  }
+  else {
+    return 0;
+  }
+}
+
+function rentalCarCost(days) {
+  return baseCost(days, 40) - discountRate(days);
+}
+*/
