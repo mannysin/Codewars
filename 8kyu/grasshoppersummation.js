037a50407228944c5ca43a3beb30e07e1cb8c3d3
mannysin <sinclair2610@gmail.com>
2019-01-10 12:57:11 -0500

    setting up environment

---
+++ 8kyu/grasshoppersummation.js
@@ -0,0 +1,44 @@
+/*
+Problem:
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+
+My approach:
+
+
+*/
+
+let summation = function (num) {
+    // Code here
+  }
+
+
+/* Test Cases
+describe('summation', function () {
+  it('should return the correct total', function () {
+    Test.assertEquals(summation(1), 1)
+    Test.assertEquals(summation(8), 36)
+  })
+})
+*/
+
+summation()
+
+/*
+Best Practice:
+
+
+
+
+
+
+
+*/
