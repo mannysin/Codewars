c941787775ab3d6e91de8602e162470ffa9e2c99
mannysin <sinclair2610@gmail.com>
2019-01-11 18:45:44 -0500

    Solution for Alternatve capitalization

--- 7kyu/alternatecapitalization.js
+++ 7kyu/alternatecapitalization.js
@@ -10,13 +10,26 @@ Good luck!
 
 My approach:
 
+Split string s.
 
+Iterate through split string with a for each loop.
+Check if odd index is divisible by 2, if so, make uppercase and push to first string array.
+Check if even index is divisible by 2, if so, make uppercase and push to second string array.
+
+
+Finally, join both string arrays result variable and return result.
 
 */
 
 function capitalize(s){
-    return [];
-    };
+    let stringArray = s.split('')
+    let resString1 = [], resString2 = []
+
+        stringArray.forEach((letter,index)=>{ index%2===0? resString1.push(letter.toUpperCase()) : resString1.push(letter)})
+        stringArray.forEach((letter,index)=>{ index%2===0? resString2.push(letter) : resString2.push(letter.toUpperCase())})
+
+            return [resString1.join(''),resString2.join('')];
+};
 
 
 /* Test Cases
@@ -28,10 +41,13 @@ Test.assertDeepEquals(capitalize("codewarriors"),['CoDeWaRrIoRs', 'cOdEwArRiOrS'
 });
 */
 
-capitalize("abcdef") // outputs: 
+capitalize("abcdef") // outputs: ['AbCdEf', 'aBcDeF']
 
 /*
 Best Practice:
 
+function capitalize(s){
+  return [0,1].map(r=>[...s].map((c,i)=>i%2===r?c.toUpperCase():c).join(''));
+};
 
 */
