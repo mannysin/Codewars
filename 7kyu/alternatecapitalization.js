f993ec27689d9b824373366e9ed419b26b51108d
mannysin <sinclair2610@gmail.com>
2019-01-11 15:59:30 -0500

    setting up environment

---
+++ 7kyu/alternatecapitalization.js
@@ -0,0 +1,37 @@
+/*
+Problem:
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+My approach:
+
+
+
+*/
+
+function capitalize(s){
+    return [];
+    };
+
+
+/* Test Cases
+describe("Basic tests", function(){
+Test.assertDeepEquals(capitalize("abcdef"),['AbCdEf', 'aBcDeF']);
+Test.assertDeepEquals(capitalize("codewars"),['CoDeWaRs', 'cOdEwArS']);
+Test.assertDeepEquals(capitalize("abracadabra"),['AbRaCaDaBrA', 'aBrAcAdAbRa']);
+Test.assertDeepEquals(capitalize("codewarriors"),['CoDeWaRrIoRs', 'cOdEwArRiOrS']);
+});
+*/
+
+capitalize("abcdef") // outputs: 
+
+/*
+Best Practice:
+
+
+*/
