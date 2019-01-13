d9a3b0d0c9e73d79640bfd9b23bc5b08703477d4
mannysin <sinclair2610@gmail.com>
2019-01-12 23:02:20 -0500

    setting up environment

---
+++ 7kyu/friendorfoe.js
@@ -0,0 +1,40 @@
+/*
+Problem:
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+Note: keep the original order of the names in the output.
+
+My approach:
+
+*/
+
+function friend(friends){
+}
+
+
+/* Test Cases
+Test.describe("Basic tests",function(){
+Test.assertSimilar(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"])
+Test.assertSimilar(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"])
+Test.assertSimilar(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"])
+Test.assertSimilar(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"])
+Test.assertSimilar(friend(["Hell", "Is", "a", "badd", "word"]), ["Hell", "badd", "word"])
+Test.assertSimilar(friend(["Issa", "Issac", "Issacs", "ISISS"]), ["Issa"])
+Test.assertSimilar(friend(["Robot", "Your", "MOMOMOMO"]), ["Your"])
+Test.assertSimilar(friend(["Hello", "I", "AM", "Sanjay", "Gupt"]), ["Gupt"])
+Test.assertSimilar(friend(["This", "IS", "enough", "TEst", "CaSe"]), ["This", "TEst", "CaSe"])
+Test.assertSimilar(friend([]), [])
+})
+*/
+
+friend(["Ryan", "Kieran", "Mark"]) // outputs:
+
+/*
+Best Practice:
+
+
+*/
