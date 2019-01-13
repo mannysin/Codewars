ef6634938d79fb526148b877c32ce89aa34f92c6
mannysin <sinclair2610@gmail.com>
2019-01-12 22:46:41 -0500

    solution for isNdivisiblebyXandY

--- 8kyu/isNdivisiblebyXandY.js
+++ 8kyu/isNdivisiblebyXandY.js
@@ -9,9 +9,23 @@ isDivisible(12,2,6)--> true because 12 is divisible by 2 and 6
 isDivisible(100,5,3)--> false because 100 is not divisible by 3
 isDivisible(12,7,5)--> false because 12 is neither divisible by 7 nor 5
 
+My approach:
+
+Set two consts for the modulus division of first num(x) and second num(y).
+Check if both divisions equal 0, if so, return true. 
+
 */
 
 function isDivisible(n, x, y) {
+    const oneNum = n % x 
+    const twoNum = n % y
+
+    if (oneNum === 0 && twoNum === 0){
+      return true
+    } else {
+      return false
+    }
+
 }
 
 
@@ -22,10 +36,13 @@ Test.assertSimilar(isDivisible(8,3,4),false);
 Test.assertSimilar(isDivisible(48,3,4),true);
 */
 
-isDivisible() // output:
+isDivisible(12, 3, 4) // output: true
 
 /*
 Best Practice:
 
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
 
 */
