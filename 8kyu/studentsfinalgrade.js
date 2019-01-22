5736a85f5cbcf6bb342b6a8c1080a237cbd404b6
mannysin <sinclair2610@gmail.com>
2019-01-21 23:38:32 -0500

    setting up environment

---
+++ 8kyu/studentsfinalgrade.js
@@ -0,0 +1,48 @@
+/*
+Problem:
+Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+
+Example:
+finalGrade(100, 12);  // 100
+finalGrade(99, 0);    // 100
+finalGrade(10, 15);   // 100
+
+finalGrade(85, 5);    // 90
+
+finalGrade(55, 3);    // 75
+
+finalGrade(55, 0);    // 0
+finalGrade(20, 2);    // 0
+
+My approach:
+
+*/
+
+function finalGrade (exam, projects) {
+    return // final grade
+}
+
+
+/* Test Cases
+describe("Sample tests", function(){
+  Test.assertEquals(finalGrade(100, 12), 100);
+  Test.assertEquals(finalGrade(85, 5), 90);
+});
+*/
+
+finalGrade(100,12) // output: 
+
+/*
+Best Practice:
+
+
+*/
