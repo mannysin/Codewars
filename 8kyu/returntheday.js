5730392e5a0fc9aabf12d5094d69a61a6caa2d1c
mannysin <sinclair2610@gmail.com>
2019-01-27 18:40:09 -0500

    Solution for Return the Day

--- 8kyu/returntheday.js
+++ 8kyu/returntheday.js
@@ -20,10 +20,36 @@ if it's not a number between 1-7 return "Wrong, please enter a number between 1 
 
 My approach:
 
+Make a switch/case statement with each num case and the corresponding day of the week with each case. Default value will be anything besides those numbers.
+
 */
 
 function whatday(num) { 
-    // put your code here
+    switch(num) {
+        case 1:
+            return "Monday"
+            break;
+        case 2:
+            return "Tuesday"
+            break;
+        case 3:
+            return "Wednesday"
+            break;
+        case 4:
+            return "Thursday"
+            break;
+        case 5:
+            return "Friday"
+            break;
+        case 6:
+            return "Saturday"
+            break;
+        case 7:
+            return "Sunday"
+            break;
+        default:
+          return "Wrong, please enter a number between 1 and 7"
+    }
 }
 
 
@@ -40,10 +66,14 @@ describe( "whatday", function(){
 });
 */
 
-whatday(4) //outputs: 
+whatday(1) //outputs: Sunday
 
 /*
 Best Practice:
 
+function whatday(num) { 
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return days[num - 1] || 'Wrong, please enter a number between 1 and 7';
+}
 
 */
