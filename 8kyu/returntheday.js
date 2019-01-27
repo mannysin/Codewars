4ab5168d52ef8d7eea1f8e173b45036160ec3cd7
mannysin <sinclair2610@gmail.com>
2019-01-27 18:26:36 -0500

    setting up environment

---
+++ 8kyu/returntheday.js
@@ -0,0 +1,49 @@
+/*
+Problem:
+Write a function called whatday() which returns the day according to the number
+
+1 returns 'Sunday'
+
+2 returns 'Monday'
+
+3 returns 'Tuesday'
+
+4 returns 'Wednesday'
+
+5 returns 'Thursday'
+
+6 returns 'Friday'
+
+7 returns 'Saturday'
+
+if it's not a number between 1-7 return "Wrong, please enter a number between 1 and 7"
+
+My approach:
+
+*/
+
+function whatday(num) { 
+    // put your code here
+}
+
+
+/* Test Cases
+describe( "whatday", function(){
+  it( "should work for the examples" , function(){
+    Test.assertEquals(whatday(1),'Sunday')
+    Test.assertEquals(whatday(2), 'Monday')
+    Test.assertEquals(whatday(3),'Tuesday')
+       Test.assertEquals(whatday(8),  'Wrong, please enter a number between 1 and 7')
+    Test.assertEquals(whatday(20),  'Wrong, please enter a number between 1 and 7')
+
+      });
+});
+*/
+
+whatday(4) //outputs: 
+
+/*
+Best Practice:
+
+
+*/
