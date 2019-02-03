3b09de66ebf89ae545418d698753e79b06d7507e
mannysin <sinclair2610@gmail.com>
2019-02-02 22:38:24 -0500

    setting up environment

---
+++ 7kyu/removeanchorfromurl.js
@@ -0,0 +1,36 @@
+/*
+Problem:
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples:
+
+// returns 'www.codewars.com'
+removeUrlAnchor('www.codewars.com#about')
+
+// returns 'www.codewars.com?page=1' 
+removeUrlAnchor('www.codewars.com?page=1')
+
+My approach:
+
+
+*/
+
+function removeUrlAnchor(url){
+    // TODO: complete
+  }
+  
+
+
+/* Test Cases
+Test.assertEquals(removeUrlAnchor('www.codewars.com#about'), 'www.codewars.com');
+
+*/
+
+removeUrlAnchor('www.codewars.com#about') // outputs:
+
+/*
+Best Practice:
+
+
+*/
