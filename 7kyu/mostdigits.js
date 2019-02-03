4306391faef2b3acac8120bc6b6ef253d24cf1e5
mannysin <sinclair2610@gmail.com>
2019-02-02 22:54:08 -0500

    setting up environment

---
+++ 7kyu/mostdigits.js
@@ -0,0 +1,32 @@
+/*
+Problem:
+
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+My approach:
+
+
+*/
+
+function findLongest(array){
+    // code here
+}
+  
+
+
+/* Test Cases
+Test.assertEquals(findLongest([1, 10, 100]), 100)
+Test.assertEquals(findLongest([9000, 8, 800]), 9000)
+Test.assertEquals(findLongest([8, 900, 500]), 900)
+
+*/
+
+findLongest([1, 10, 100]) // outputs:
+
+/*
+Best Practice:
+
+
+*/
